ab1b61451820de9a2886a52186323e73c855a767
nibnualdi <nibnualdi@gmail.com>
2025-07-14 06:34:58 +0700

    fix: auth

--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [App\Http\Controllers\TicketController::class, 'index'])->name('ticket.index');
-Route::post('/', [App\Http\Controllers\TicketController::class, 'store'])->name('ticket.store');
+Route::get('/', [App\Http\Controllers\TicketController::class, 'index'])->name('ticket.index')->middleware('auth');
+Route::post('/', [App\Http\Controllers\TicketController::class, 'store'])->name('ticket.store')->middleware('auth');
 
 Auth::routes();
 
-Route::get('/tickets/{id}', [App\Http\Controllers\TicketController::class, 'edit'])->name('ticket.edit');
-Route::put('/tickets/{id}', [App\Http\Controllers\TicketController::class, 'editAction'])->name('ticket.editAction');
-Route::get('/', [App\Http\Controllers\TicketController::class, 'filterByStatusDateCategory'])->name('ticket.filterByStatusDateCategory');
+Route::get('/tickets/{id}', [App\Http\Controllers\TicketController::class, 'edit'])->name('ticket.edit')->middleware('auth');
+Route::put('/tickets/{id}', [App\Http\Controllers\TicketController::class, 'editAction'])->name('ticket.editAction')->middleware('auth');
+Route::get('/', [App\Http\Controllers\TicketController::class, 'filterByStatusDateCategory'])->name('ticket.filterByStatusDateCategory')->middleware('auth');
